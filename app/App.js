cd801ac256b2aad00802eb99d7188a37cdb138d9
ebtesam25 <ebtesam@iut-dhaka.edu>
2021-03-25 23:08:38 +0300

    init

---
+++ app/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Welcome from './src/screens/welcome';
+
+
+
+const Stack = createStackNavigator();
+
+
+function MyStack() {
+  return (
+    <Stack.Navigator
+      initialRouteName="Welcome"
+    >
+      <Stack.Screen 
+        name="Welcome" 
+        component={Welcome} 
+        options={{ headerShown: false}} 
+      />
+      
+    
+    
+     
+      
+    </Stack.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
+  );
+}
